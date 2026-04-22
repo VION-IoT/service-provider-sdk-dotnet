4bf61455d01182f29c96579c14b2cc97090449e0
Manuel Stüssi <manuel.stuessi@ecocoach.com>
2026-04-22 09:41:47 +0200

    Add JsonStringEnumConverter for LogLevel serialization
    Enable string-based serialization for LogLevel enum in ServiceProviderJsonContext using JsonStringEnumConverter. Bump version to 0.1.3 and update release notes accordingly.

--- Vion.ServiceProvider.Sdk/JsonSerializationContexts/ServiceProviderJsonContext.cs
+++ Vion.ServiceProvider.Sdk/JsonSerializationContexts/ServiceProviderJsonContext.cs
@@ -1,8 +1,9 @@
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
+using Microsoft.Extensions.Logging;
 using Shared.Contracts.Events.CloudToMesh;
 using Shared.Contracts.Events.MeshToCloud;
 using Shared.Contracts.Events.ServiceProviderToMesh;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Vion.ServiceProvider.Sdk.RegistrationFlow;
 
 // ReSharper disable PartialTypeWithSinglePart this must be a partial class because it is generated
@@ -20,6 +21,8 @@ namespace Vion.ServiceProvider.Sdk.JsonSerializationContexts
     [JsonSerializable(typeof(Dictionary<string, object>))]
     [JsonSerializable(typeof(string))]
     [JsonSerializable(typeof(bool))]
-    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, DictionaryKeyPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+                                 DictionaryKeyPolicy = JsonKnownNamingPolicy.CamelCase,
+                                 Converters = [typeof(JsonStringEnumConverter<LogLevel>)])]
     public partial class ServiceProviderJsonContext : JsonSerializerContext;
 }
